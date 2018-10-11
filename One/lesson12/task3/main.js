1ea0acfa5bd2f1de0af6344f7b5d403a571e1268
Anton <anton_dv@list.ru>
2018-10-11 10:55:47 +0300

    Update main.js

--- One/lesson12/task3/main.js
+++ One/lesson12/task3/main.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const husband1 = new human;
 
 try {
-    fs.appendFileSync('loh.txt', husband1.getSalary());
+    fs.appendFileSync('log.txt', husband1.getSalary());
     fs.appendFileSync('log.txt', husband1.listFriends());
     fs.appendFileSync('log.txt', husband1.ifSober());
 
