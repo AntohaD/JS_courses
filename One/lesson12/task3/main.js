b3253bc0e3bc58263038fbdaf150a1945fd46471
Anton <anton_dv@list.ru>
2018-10-11 10:54:37 +0300

    write in the log file

--- One/lesson12/task3/main.js
+++ One/lesson12/task3/main.js
@@ -1,11 +1,16 @@
-
-
 const human = require('./husband').mod;
 
 const fs = require('fs');
 
 const husband1 = new human;
 
-fs.appendFileSync('log.txt', husband1.getSalary());
-fs.appendFileSync('log.txt', husband1.listFriends());
-fs.appendFileSync('log.txt', husband1.ifSober());
+try {
+    fs.appendFileSync('loh.txt', husband1.getSalary());
+    fs.appendFileSync('log.txt', husband1.listFriends());
+    fs.appendFileSync('log.txt', husband1.ifSober());
+
+    console.log('Added to the log file');
+
+} catch (error) {
+    console.log(error);
+}
